fdd926cda34d9a306623d9a7f9f1d7f1ad5afaad
vigneshv1999 <vigneshv1452@gmail.com>
2022-07-21 13:03:52 +0000

    feat:connected to database

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=postgres --no-align --tuples-only -c"
 echo "Enter your username:"
-read n
-echo "Hello $n"
+read "n"
+a1="$($PSQL "select count(*),min(gs_ct) from games where name='$n' group by name")"
+if [[ -z $a1 ]]
+then
+  echo "Welcome, $n! It looks like this is your first time here."
+else
+  echo "$a1" | while IFS='|' read c m
+  do
+    echo "Welcome back, $n! You have played $c games, and your best game took $m guesses."
+  done
+fi
