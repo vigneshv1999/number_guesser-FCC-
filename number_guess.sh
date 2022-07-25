b89c3505089be70e772d48f94243d33fd6d36f7c
vigneshv1999 <vigneshv1452@gmail.com>
2022-07-25 03:48:09 +0000

    feat:my tests wont pass so trying forums code

--- number_guess.sh
+++ number_guess.sh
@@ -1,38 +1,67 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=postgres -t --no-align -c"
-echo "Enter your username:"
-read n
-a1="$($PSQL "select name,count(*),min(gs_ct) from games where name='$n' group by name")"
-if [[ -z $a1 ]]
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+NUMBER=$((RANDOM % 1000 + 1))
+NUMBER_OF_GUESSES=0
+GAMES_PLAYED=0
+
+GUESS_LOOP() {
+read GUESS
+
+if [[ $GUESS =~ ^[0-9]+$ ]]
 then
-  echo "Welcome, $n! It looks like this is your first time here."
-else
-  echo "$a1" | while IFS='|' read n1 c m
-  do
-    echo "Welcome back, $n1! You have played $c games, and your best game took $m guesses."
-  done
-fi
-r=$(($RANDOM%1000))
-MAIN_MENU(){
-  echo "Guess the secret number between 1 and 1000:"
-  read g
-  if [[ $g =~ [0-9] ]]
+  if [[ $GUESS = $NUMBER ]]
   then
-    if [[ $g -lt $r ]]
+    let NUMBER_OF_GUESSES++
+
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $NUMBER. Nice job!"
+
+    #Update best game yet
+    BEST_GAME=$($PSQL "SELECT best_game FROM guesser WHERE username = '$USERNAME'")
+     if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
     then
-      echo "It's higher than that, guess again:"
-      MAIN_MENU $(($1+1)) 
-    elif [[ $g -gt $r ]]
-    then
-      echo "It's lower than that, guess again:"
-      MAIN_MENU $(($1+1))
-    else
-      echo "You guessed it in $1 tries. The secret number was $r. Nice job!"
+      UPDATE_BEST_GAME_RESULT=$($PSQL "UPDATE guesser SET best_game = '$NUMBER_OF_GUESSES' WHERE username = '$USERNAME'")
     fi
-  else
-    echo "That is not an integer, guess again:"
-    MAIN_MENU $(($1+1))
+      
+  else if [[ $GUESS > $NUMBER ]]
+    then
+      let NUMBER_OF_GUESSES++
+      echo "It's lower than that, guess again:"
+      GUESS_LOOP
+    else 
+      let NUMBER_OF_GUESSES++
+      echo "It's higher than that, guess again:"
+      GUESS_LOOP
+    fi
   fi
+else
+  echo "That is not an integer, guess again:"
+  GUESS_LOOP
+fi
 }
 
-MAIN_MENU 1
+echo "Enter your username:"
+read USERNAME
+
+GUESSER=$($PSQL "SELECT * FROM guesser WHERE username = '$USERNAME'")
+
+if [[ -z $GUESSER ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO guesser(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+else
+  echo "$GUESSER" | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done  
+fi
+
+#Update number of games played
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM guesser WHERE username = '$USERNAME'")
+let GAMES_PLAYED++   
+UPDATE_GAMES_PLAYED_RESULT="$($PSQL "UPDATE guesser SET games_played = games_played + 1 WHERE username = '$USERNAME'")"
+
+echo "Guess the secret number between 1 and 1000:"
+
+GUESS_LOOP
